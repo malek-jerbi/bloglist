5adbb4191db53f83eb9490e816b785ed79bfadbe
Malek Jerbi <malek.jerbi@gmail.com>
2021-10-15 15:00:39 -0400

    added controllers and models

--- index.js
+++ index.js
@@ -5,15 +5,7 @@ const cors = require('cors')
 const mongoose = require('mongoose')
 const config= require('./utils/config')
 const logger=require('./utils/logger')
-
-const blogSchema = new mongoose.Schema({
-  title: String,
-  author: String,
-  url: String,
-  likes: Number
-})
-
-const Blog = mongoose.model('Blog', blogSchema)
+const blogsRouter=require('./controllers/blogs')
 
 const mongoUrl = config.URL
 
@@ -21,24 +13,7 @@ mongoose.connect(mongoUrl)
 
 app.use(cors())
 app.use(express.json())
-
-app.get('/api/blogs', (request, response) => {
-  Blog
-    .find({})
-    .then(blogs => {
-      response.json(blogs)
-    })
-})
-
-app.post('/api/blogs', (request, response) => {
-  const blog = new Blog(request.body)
-
-  blog
-    .save()
-    .then(result => {
-      response.status(201).json(result)
-    })
-})
+app.use('/api/blogs', blogsRouter)
 
 app.listen(config.PORT, () => {
   logger.info(`Server running on port ${config.PORT}`)
