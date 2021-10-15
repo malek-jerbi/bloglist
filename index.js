6fe2246f0030422852a513967b7b8b150323b03c
Malek Jerbi <malek.jerbi@gmail.com>
2021-10-15 14:40:31 -0400

    added logger and config

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const express = require('express')
 const app = express()
 const cors = require('cors')
 const mongoose = require('mongoose')
+const config= require('./utils/config')
+const logger=require('./utils/logger')
 
 const blogSchema = new mongoose.Schema({
   title: String,
@@ -13,7 +15,8 @@ const blogSchema = new mongoose.Schema({
 
 const Blog = mongoose.model('Blog', blogSchema)
 
-const mongoUrl = 'mongodb+srv://fullstack:database123@cluster0.clza2.mongodb.net/blogs?retryWrites=true&w=majority'
+const mongoUrl = config.URL
+
 mongoose.connect(mongoUrl)
 
 app.use(cors())
@@ -37,7 +40,6 @@ app.post('/api/blogs', (request, response) => {
     })
 })
 
-const PORT = 3003
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
+app.listen(config.PORT, () => {
+  logger.info(`Server running on port ${config.PORT}`)
 })
